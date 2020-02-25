f77ba2322a4c9240009e265c90e4d3ea356cf7a1
Simon Lervad <lervad1996@gmail.com>
2020-02-25 12:57:07 +0100

    push

--- world/routes/index.js
+++ world/routes/index.js
@@ -13,6 +13,32 @@ router.get('/continents', function(req, res, next) {
 	res.render('continents', newobj);
 });
 
+router.get('/addNew', function(req, res, next) {
+	let newobj = { title: 'Add new'};
+	res.render('addNew', newobj);
+});
+
+router.post('/addNewCountry', function(req, res, next) {
+    const mongo = require('mongodb');
+    const dbname = "world";
+    const constr = `mongodb://localhost:27017`;
+    console.log(req.body);
+    let query = { name: req.body.name };
+    mongo.connect(constr, { useNewUrlParser: true, useUnifiedTopology: true}, function (error, con) {
+        if (error) {
+            throw error;
+        }
+        const db = con.db(dbname);
+        db.collection("country").updateOne(query, {"$set": req.body}, {upsert: true}, function (err, collection) {
+            if (err) {
+                throw err;
+            }
+            let newobj = req.body;
+		    res.render('addedCountry', { title: 'Country inserted', newobj});
+        });
+    });
+});
+
 router.get('/countries', function(req, res, next) {
         const mongo = require('mongodb');
         const dbname = "world";
@@ -35,7 +61,7 @@ router.get('/countries', function(req, res, next) {
 		                if (err) {
 		                    throw err;
 		                }
-		                obj = { country: 'Countries', city: 'Cities', lang: "languages", data, dataTwo, dataThree};
+		                obj = { title: 'Countries', country: 'Countries', city: 'Cities', lang: "languages", data, dataTwo, dataThree};
 		                res.render('countries', obj);
 		            });
 	            });
